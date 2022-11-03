e916cf55933f18979a54b5fcf8a36c06f87f7d1a
somo2003 <somo2003@colorado.edu>
2022-11-03 16:46:13 -0600

    this is my second commit

--- js/assignment11.js
+++ js/assignment11.js
@@ -1,21 +1,25 @@
 const quoteButton = document.querySelector('.new-quote');
 quoteButton.addEventListener('click', getQuote);
 
-const endpoint = 'http://movie-quotes-app.herokuapp.com/api';
+// const endpoint = 'https://date.nager.at/api/v2/publicholidays/2020/US';
 
 async function getQuote() 
 {
-    let text = await fetch(endpoint);
-    let response = await text.text();
+    // let text = await fetch(endpoint);
+    // let response = await text.text();
 
-    let json_response = JSON.parse(response);
+    // let json_response = JSON.parse(response);
 
-    displayQuote(json_response['message']);
+    // displayQuote(json_response['message']);
+
+    fetch("https://dog.ceo/api/breeds/image/random")
+    .then(response => response.json())
+    .then(data => displayQuote(data.message));
 }
 
 function displayQuote(x)
 {
-    document.getElementById('js-quote-text').textContent = x;
+    document.getElementById('picture').src = x;
 }
 
 getQuote();
